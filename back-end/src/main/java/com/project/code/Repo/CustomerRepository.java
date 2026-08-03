50c6d6ce04e8d13d8193aeb11ea133fd857c36a8
shootjulian <julian.gomez2@estudiantesunibague.edu.co>
2026-08-03 14:55:46 -0500

    Declaring findByEmail method for CustomerRepository

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -1,7 +1,13 @@
 package com.project.code.Repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.code.Model.*;
 
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+    
+    public Customer findByEmail(String email);
+
+
 // 1. Add the repository interface:
 //    - Extend JpaRepository<Customer, Long> to inherit basic CRUD functionality.
 //    - This allows the repository to perform operations like save, delete, update, and find without having to implement these methods manually.
