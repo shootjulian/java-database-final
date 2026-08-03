1c1cfbf600dcb104a805b266ac8983b200cc95e6
shootjulian <julian.gomez2@estudiantesunibague.edu.co>
2026-08-03 15:04:56 -0500

    Finished CustomerRepository methods

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -1,12 +1,17 @@
 package com.project.code.Repo;
 
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.project.code.Model.*;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
     
     public Customer findByEmail(String email);
 
+    public List<Customer> findByName(String name);
 
 // 1. Add the repository interface:
 //    - Extend JpaRepository<Customer, Long> to inherit basic CRUD functionality.
